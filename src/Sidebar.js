dfc4926916ce662de07f9e4163f6f2c5d8f50818
Mahip-Jain <mahipjain2008@gmail.com>
2022-03-18 11:20:20 +0530

    initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,44 @@
+function Sidebar({
+  notes,
+  onAddNote,
+  onDeleteNote,
+  activeNote,
+  setActiveNote,
+}) {
+  const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
+
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>Notes</h1>
+        <button onClick={onAddNote}>Add</button>
+      </div>
+      <div className="app-sidebar-notes">
+        {sortedNotes.map((note) => (
+          <div
+            className={`app-sidebar-note ${note.id === activeNote && "active"}`} // checks if this note is active and adds the active class
+            onClick={() => setActiveNote(note.id)} // this sets the active note to the noteid
+          >
+            <div className="sidebar-note-title">
+              <strong>{note.title}</strong>
+              <button onClick={() => onDeleteNote(note.id)}>Delete</button>
+            </div>
+
+            <p>{note.body && note.body.substr(0, 100) + "..."}</p>
+
+            <small className="note-meta">
+              Last modified{" "}
+              {new Date(note.lastModified).toLocaleDateString("en-GB", {
+                // format date
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
